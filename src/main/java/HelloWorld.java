f3c17adde893ed32df94e798c331322920a93531
Jingjing Wang <steady0707@gmail.com>
2020-11-13 23:28:20 -0600

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
         int[] array = new int[]{0,1};
-        System.out.print("Hello");
+        System.out.print("Hello world");
     }
 }
