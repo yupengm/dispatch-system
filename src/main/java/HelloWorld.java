4bb529e922ca74cbfde920aec50ee651fb3e0896
8700nd <rossilyx@gmail.com>
2020-11-19 18:42:41 -0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){
         int[] array = new int[]{0,1};
         System.out.print("Hello");
+        System.out.println(array[0]);
     }
 }
