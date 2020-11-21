02b1d8d3e4b1a6f9cb07048c3d161126dc2c020c
cj3699xiao <354123302@qq.com>
2020-11-21 01:19:22 -0600

    Create Map BE Client
    Could send out request and get inputStream back. Trying to figure out how to get geolocation out of inputStream

--- src/main/java/com/dispatch/external/GoogleMapClient.java
+++ src/main/java/com/dispatch/external/GoogleMapClient.java
@@ -1,16 +1,83 @@
 package com.dispatch.external;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.util.*;
 
 public class GoogleMapClient {
-    final String Key = "AIzaSyBuoOrk0v4p_2Q-Pb7xymPwQoJvDP9v-ck" ;
+    private final static String KEY = "AIzaSyBuoOrk0v4p_2Q-Pb7xymPwQoJvDP9v-ck" ;
+    private static final String EXTRACT_URL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=";
+    private static final String FORMAT_URL = "&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=";
+
 
     String test = "900 S Clark, Chicago, IL";
     //The following example shows a Find Place request for "Museum of Contemporary Art Australia",
     // including the photos, formatted_address, name, rating, opening_hours, and geometry fields:
     String request = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY";
 
-    String request1 = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=900%20S%20Clark%20Chicago%20IL&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY";
+    public static String request1 = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=900%20S%20Clark%20Chicago%20IL&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY";
 
     //189 Prairie Run, Edmond, OK
     String request2 = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=189%20Prairie%20Run%20Edmond%20OK&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY";
 
+    public void getLocation(String input) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        StringBuilder URL = new StringBuilder(EXTRACT_URL);
+        input = input.replace(",","");
+
+        // format URL
+        for(String ele: input.split(" ")) {
+            URL.append(ele);
+            URL.append("%20");
+        }
+        URL.delete(URL.length() - 3,URL.length());
+        URL.append(FORMAT_URL);
+        URL.append(KEY);
+//        System.out.println(URL);
+
+        //
+        ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+            @Override
+            public String handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+                if (response.getStatusLine().getStatusCode() != 200) {
+                    return "empty";
+                }
+                HttpEntity entity = response.getEntity();
+
+                if (entity == null) {
+                    return "empty";
+                }
+
+                return entity.getContent().toString();
+            }
+        };
+
+        HttpPost request = new HttpPost(URL.toString());
+        request.setHeader("Content-type", "application/json");
+
+        try {
+            char[] all = httpClient.execute(request, responseHandler).toCharArray();
+//            System.out.println();
+            for(char cur : all) {
+                System.out.print(cur);
+            }
+            httpClient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        GoogleMapClient a = new GoogleMapClient();
+        a.getLocation("900 S Clark, Chicago, IL");
+//        System.out.println(request1);
+    }
 }
