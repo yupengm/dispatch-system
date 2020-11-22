222ed0a5554f3c90e10c7b647f31341e0493b6d8
8700nd <rossilyx@gmail.com>
2020-11-22 15:23:09 -0800

    Added map

--- src/ui/src/components/Map.js
+++ src/ui/src/components/Map.js
@@ -1,12 +1,77 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { withGoogleMap, GoogleMap, withScriptjs, InfoWindow, Marker } from "react-google-maps";
+import Geocode from "react-geocode";
+import Autocomplete from 'react-google-autocomplete';
+import { Descriptions } from 'antd';
+import {useGoogleMap} from "@react-google-maps/api";
 
-class Map extends Component {
-    render(){
-        return(
-            <div className="map-box">
+const { MarkerWithLabel } = require("react-google-maps/lib/components/addons/MarkerWithLabel");
 
-            </div>
+class Map extends React.Component {
+
+    state = {
+        address: '',
+        city: '',
+        area: '',
+        state: '',
+        zoom: 13,
+        height: 400,
+    }
+
+    render() {
+        const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+
+            <GoogleMap
+                defaultZoom={this.state.zoom}
+                defaultCenter={{ lat: 37.763880, lng: -122.446083 }}
+                disableDefaultUI={true}
+            >
+                <Marker
+                    position={{ lat: 37.78741078914182, lng: -122.43674218604595 }}
+                >
+                    <InfoWindow>
+                        <div>
+                            Station 1
+                        </div>
+                    </InfoWindow>
+                </Marker>
+
+                <Marker
+                    position={{ lat: 37.74575075621106, lng: -122.43330895872147 }}
+                >
+                    <InfoWindow>
+                        <div>
+                            Station 2
+                        </div>
+                    </InfoWindow>
+                </Marker>
+
+                <Marker
+                    position={{ lat: 37.76475172762295, lng: -122.48394906175754 }}
+                >
+                    <InfoWindow>
+                        <div>
+                            Station 3
+                        </div>
+                    </InfoWindow>
+                </Marker>
+
+            </GoogleMap>
+        ));
+
+        return (
+            <MapWithAMarker
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=APIKEY&libraries=geometry,drawing,places"
+                loadingElement={<div className="map" />}
+                containerElement={<div className="map" />}
+                mapElement={<div className="map" />}
+            />
         )
     }
+
+    addMarker = (location, map) => {
+
+    }
+
 }
 export default Map;
