02dda35233c973ab848dadaf5b44dfb259ed4cf9
Jingjing Wang <steady0707@gmail.com>
2020-11-21 22:43:03 -0600

    ReactInstall

---
+++ src/ui/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <p className="title">
+                    Dispatch
+                </p>
+            </header>
+        );
+    }
+}
+
+export default Header;
