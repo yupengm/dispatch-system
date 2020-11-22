498388185eb7bb9d93f3957d5daac397e3b16710
ruomindaniel <ruomindaniel@gmail.com>
2020-11-21 23:46:26 -0500

    new

---
+++ src/ui/components/Main.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div className="main">
+                <div className="left-side">
+                    left side
+                </div>
+                <div className="right-side">
+                    right side
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Main;
