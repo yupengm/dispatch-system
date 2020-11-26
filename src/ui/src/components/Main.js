5ea2dadba62eeea671fb4302a754fc3a47cb090e
8700nd <rossilyx@gmail.com>
2020-11-25 16:34:51 -0800

    points cannot update

--- src/ui/src/components/Main.js
+++ src/ui/src/components/Main.js
@@ -3,17 +3,35 @@ import UserSetting from "./UserInput";
 import Map from"./Map"
 import UserAddress from "./UserAddress";
 class Main extends Component {
+    constructor() {
+        super();
+        this.state = {
+            destination: "",
+            target: ""
+        }
+    }
+    setPoints = (destination, target) => {
+        console.log('main setpoints', this.state)
+        this.setState(() => ({
+            destination: destination,
+            target: target
+        }));
+        console.log('setPoints changed', this.state)
+    }
+
     render(){
+        const {destination, target} = this.state
+        console.log('main render', destination)
         return (
             <div className='main'>
                 <div className="left-side">
                         <UserSetting />
                         <div className="address">
-                            <UserAddress />
+                            <UserAddress showPoints={this.setPoints}/>
                         </div>
                 </div>
                 <div>
-                    <Map />
+                    <Map des={destination} tar={target}/>
                 </div>
             </div>
         );
--- src/ui/src/components/Map.js
+++ src/ui/src/components/Map.js
@@ -1,34 +1,54 @@
 import React, { Component } from "react";
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng,} from 'react-places-autocomplete';
+import UserAddress from './UserAddress';
 
 export class MapContainer extends Component {
-    constructor() {
-        super();
-        this.state ={
-            address: '',
-            showingInfoWindow: false,
-            activeMarker: {},
-            selectedPlace: {},
+    constructor(props) {
+        super(props);
+        this.state = {
+            // showingInfoWindow: false,
+            // activeMarker: {},
+            // selectedPlace: {},
             mapCenter: {lat: 37.763880, lng: -122.446083},
+            zoom: 13,
             station1: {lat: 37.78741078914182, lng: -122.43674218604595},
             station2: {lat: 37.74575075621106, lng: -122.43330895872147},
-            station3: {lat: 37.76475172762295, lng: -122.48394906175754}
+            station3: {lat: 37.76475172762295, lng: -122.48394906175754},
+            destination: {lat: 0, lng: 0},
+            target: {lat: 0, lng: 0}
         };
-    };
+    }
 
-    handleChange = address => {
-        this.setState({ address });
-    };
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(1)
+        if (prevProps.des !== this.props.des) {
+            console.log(2)
+            this.locatePoint();
+        }
+    }
 
-    handleSelect = address => {
-        geocodeByAddress(address)
+
+    locatePoint = () => {
+        const { des } = this.props;
+        const { tar } = this.props;
+        console.log('locatePoint', this.props.des)
+        console.log('locatePoint', des)
+
+        this.setState({ des, tar });
+        geocodeByAddress(des)
             .then(results => getLatLng(results[0]))
             .then(latLng => {
-                console.log('Success', latLng)
-                this.setState({ address })
-                this.setState({ mapCenter: latLng })
-                console.log(this.state.mapCenter)
+                console.log('Success', latLng);
+                this.setState( {destination: latLng });
+            })
+            .catch(error => console.error('Error', error));
+
+        geocodeByAddress(tar)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => {
+                console.log('Success', latLng);
+                this.setState( {target: latLng });
             })
             .catch(error => console.error('Error', error));
     };
@@ -36,14 +56,57 @@ export class MapContainer extends Component {
     render() {
         return (
             <div className="mapWrapper" style={{height: `87%`, width: `69.5%`}}>
+                <PlacesAutocomplete
+                    value={this.locatePoint}
+                    // onChange={this.handleChange}
+                    // onSelect={this.locatePoint}
+                >
+                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                        <div>
+                            {/*<input*/}
+                            {/*    {...getInputProps({*/}
+                            {/*        placeholder: 'Search Places ...',*/}
+                            {/*        className: 'location-search-input',*/}
+                            {/*    })}*/}
+                            {/*/>*/}
+                            {/*<div className="autocomplete-dropdown-container">*/}
+                            {/*    {loading && <div>Loading...</div>}*/}
+                            {/*    {suggestions.map(suggestion => {*/}
+                            {/*        const className = suggestion.active*/}
+                            {/*            ? 'suggestion-item--active'*/}
+                            {/*            : 'suggestion-item';*/}
+                            {/*        // inline style for demonstration purpose*/}
+                            {/*        const style = suggestion.active*/}
+                            {/*            ? { backgroundColor: '#fafafa', cursor: 'pointer' }*/}
+                            {/*            : { backgroundColor: '#ffffff', cursor: 'pointer' };*/}
+                            {/*        return (*/}
+                            {/*            <div*/}
+                            {/*                {...getSuggestionItemProps(suggestion, {*/}
+                            {/*                    className,*/}
+                            {/*                    style,*/}
+                            {/*                })}*/}
+                            {/*            >*/}
+                            {/*                <span>{suggestion.description}</span>*/}
+                            {/*            </div>*/}
+                            {/*        );*/}
+                            {/*    })}*/}
+                            {/*</div>*/}
+                        </div>
+                    )}
+                </PlacesAutocomplete>
                 <Map
                     google={this.props.google}
-                    zoom={13}
+                    zoom={this.state.zoom}
                     disableDefaultUI={true}
                     initialCenter={ {
                         lat: this.state.mapCenter.lat,
                         lng: this.state.mapCenter.lng
-                    }}>
+                    }}
+                    center={{
+                        lat: this.state.mapCenter.lat,
+                        lng: this.state.mapCenter.lng
+                    }}
+                >
                     <Marker
                         position={{
                             lat: this.state.station1.lat,
@@ -59,6 +122,19 @@ export class MapContainer extends Component {
                             lat: this.state.station3.lat,
                             lng: this.state.station3.lng
                         }}/>
+
+                    <Marker
+                        position={{
+                            lat: this.state.destination.lat,
+                            lng: this.state.destination.lng
+                        }}/>
+
+                    <Marker
+                        position={{
+                            lat: this.state.target.lat,
+                            lng: this.state.target.lng
+                        }}/>
+
                 </Map>
             </div>
         )
