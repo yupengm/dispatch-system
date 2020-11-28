1e612fd0921fec7bba1ef55bdd41c64652e27dc3
8700nd <rossilyx@gmail.com>
2020-11-27 20:12:57 -0800

    before ajax

--- src/ui/src/components/Map.js
+++ src/ui/src/components/Map.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng,} from 'react-places-autocomplete';
-import UserAddress from './UserAddress';
+import customMarker from '../assets/images/flag.png';
 
 export class MapContainer extends Component {
     constructor(props) {
@@ -15,19 +15,18 @@ export class MapContainer extends Component {
             station1: {lat: 37.78741078914182, lng: -122.43674218604595},
             station2: {lat: 37.74575075621106, lng: -122.43330895872147},
             station3: {lat: 37.76475172762295, lng: -122.48394906175754},
-            destination: {lat: 0, lng: 0},
-            target: {lat: 0, lng: 0}
+            destination: {lat: null, lng: null},
+            target: {lat: null, lng: null}
         };
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         console.log(1)
-        if (prevProps.des !== this.props.des) {
+        if (prevProps.des !== this.props.des || prevProps.tar !== this.props.tar) {
             console.log(2)
             this.locatePoint();
         }
     }
-
 
     locatePoint = () => {
         const { des } = this.props;
@@ -63,34 +62,6 @@ export class MapContainer extends Component {
                 >
                     {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
                         <div>
-                            {/*<input*/}
-                            {/*    {...getInputProps({*/}
-                            {/*        placeholder: 'Search Places ...',*/}
-                            {/*        className: 'location-search-input',*/}
-                            {/*    })}*/}
-                            {/*/>*/}
-                            {/*<div className="autocomplete-dropdown-container">*/}
-                            {/*    {loading && <div>Loading...</div>}*/}
-                            {/*    {suggestions.map(suggestion => {*/}
-                            {/*        const className = suggestion.active*/}
-                            {/*            ? 'suggestion-item--active'*/}
-                            {/*            : 'suggestion-item';*/}
-                            {/*        // inline style for demonstration purpose*/}
-                            {/*        const style = suggestion.active*/}
-                            {/*            ? { backgroundColor: '#fafafa', cursor: 'pointer' }*/}
-                            {/*            : { backgroundColor: '#ffffff', cursor: 'pointer' };*/}
-                            {/*        return (*/}
-                            {/*            <div*/}
-                            {/*                {...getSuggestionItemProps(suggestion, {*/}
-                            {/*                    className,*/}
-                            {/*                    style,*/}
-                            {/*                })}*/}
-                            {/*            >*/}
-                            {/*                <span>{suggestion.description}</span>*/}
-                            {/*            </div>*/}
-                            {/*        );*/}
-                            {/*    })}*/}
-                            {/*</div>*/}
                         </div>
                     )}
                 </PlacesAutocomplete>
@@ -98,7 +69,7 @@ export class MapContainer extends Component {
                     google={this.props.google}
                     zoom={this.state.zoom}
                     disableDefaultUI={true}
-                    initialCenter={ {
+                    initialCenter={{
                         lat: this.state.mapCenter.lat,
                         lng: this.state.mapCenter.lng
                     }}
@@ -124,12 +95,18 @@ export class MapContainer extends Component {
                         }}/>
 
                     <Marker
+                        icon={{
+                            url: customMarker,
+                        }}
                         position={{
                             lat: this.state.destination.lat,
                             lng: this.state.destination.lng
                         }}/>
 
                     <Marker
+                        icon={{
+                            url: customMarker,
+                        }}
                         position={{
                             lat: this.state.target.lat,
                             lng: this.state.target.lng
