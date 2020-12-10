279e715f44914ebf5a63f7f0bdf9f054fe4f3dc1
cj3699xiao <354123302@qq.com>
2020-12-09 23:47:00 -0600

    aaaaa

--- src/main/java/com/dispatch/ApplicationConfig.java
+++ src/main/java/com/dispatch/ApplicationConfig.java
@@ -27,9 +27,14 @@ public class ApplicationConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://laijobproject.curn8ohpuukh.us-east-2.rds.amazonaws.com:3306/dispatch?createDatabaseIfNotExist=true&serverTimezone=UTC");
+//        dataSource.setUrl("jdbc:mysql://laijobproject.curn8ohpuukh.us-east-2.rds.amazonaws.com:3306/dispatch?createDatabaseIfNotExist=true&serverTimezone=UTC");
+//        dataSource.setUsername("admin");
+//        dataSource.setPassword("20MloveW");
+
+        //my
+        dataSource.setUrl("jdbc:mysql://laiproject-instance.cnwcffxp4rna.us-east-2.rds.amazonaws.com:3306/dispatch?createDatabaseIfNotExist=true&serverTimezone=UTC");
         dataSource.setUsername("admin");
-        dataSource.setPassword("20MloveW");
+        dataSource.setPassword(".12345678");
         return dataSource;
     }
 
