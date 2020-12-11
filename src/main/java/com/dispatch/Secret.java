71033ef2cf7d9514fca6070c1c04e07acb394e7c
cj3699xiao <354123302@qq.com>
2020-12-10 20:51:19 -0600

    aaa

---
+++ src/main/java/com/dispatch/Secret.java
@@ -0,0 +1,8 @@
+package com.dispatch;
+
+public class Secret {
+    static String DB_URL = "jdbc:mysql://laiproject-instance.cnwcffxp4rna.us-east-2.rds.amazonaws.com:3306/dispatch?createDatabaseIfNotExist=true&serverTimezone=UTC";
+    static String DB_USERNAME = "admin";
+    static String DB_PASSWORD = ".12345678";
+
+}
