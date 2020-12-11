8fd005a9adde05c417f742d93d692dc03c6eb5a2
yupengm <yupengm@umich.edu>
2020-12-11 01:33:33 -0500

    Create Secretes.java

---
+++ src/main/java/com/dispatch/Secretes.java
@@ -0,0 +1,7 @@
+package com.dispatch;
+
+public class Secretes {
+    static String datasourceUrl = "jdbc:mysql://laiproject-instance.ckldcu5mwgpe.us-east-2.rds.amazonaws.com:3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC";
+    static String datasourceUsername = "admin";
+    static String datasourcePassword = "Myp4130596";
+}
