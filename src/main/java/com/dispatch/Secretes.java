c00d530a83b5387cf5c99a01d7d812a59aa5dc65
cj3699xiao <354123302@qq.com>
2020-12-13 22:48:24 -0600

    deleted S

--- src/main/java/com/dispatch/Secretes.java
+++
@@ -1,7 +0,0 @@
-package com.dispatch;
-
-public class Secretes {
-    static String datasourceUrl = "jdbc:mysql://laijobproject.curn8ohpuukh.us-east-2.rds.amazonaws.com:3306/Dispatch?createDatabaseIfNotExist=true&serverTimezone=UTC";
-    static String datasourceUsername = "admin";
-    static String datasourcePassword = "20MloveW";
-}
