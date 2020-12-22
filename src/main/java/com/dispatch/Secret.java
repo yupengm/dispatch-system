1de7cc35539bf812459e4550234def43e4b286ed
yupengm <yupengm@umich.edu>
2020-12-22 01:54:07 -0500

    update .gitignore

--- src/main/java/com/dispatch/Secret.java
+++
@@ -1,9 +0,0 @@
-package com.dispatch;
-
-public class Secret {
-    static String DB_URL = "jdbc:mysql://laiproject-instance.cnwcffxp4rna.us-east-2.rds.amazonaws.com:3306/dispatch?createDatabaseIfNotExist=true&serverTimezone=UTC";
-    static String DB_USERNAME = "admin";
-    static String DB_PASSWORD = ".12345678";
-
-    static String GoogleKEY = "AIzaSyBuoOrk0v4p_2Q-Pb7xymPwQoJvDP9v-ck";
-}
--- src/main/java/com/dispatch/Secretes.java
+++ src/main/java/com/dispatch/Secretes.java
@@ -1,7 +1,8 @@
+
 package com.dispatch;
 
 public class Secretes {
-    static String datasourceUrl = "jdbc:mysql://laiproject-instance.ckldcu5mwgpe.us-east-2.rds.amazonaws.com:3306/ecommerce?createDatabaseIfNotExist=true&serverTimezone=UTC";
-    static String datasourceUsername = "admin";
-    static String datasourcePassword = "Myp4130596";
+    static String datasourceUrl = "xxx";
+    static String datasourceUsername = "xxx";
+    static String datasourcePassword = "xxx";
 }
